23593a4f6d6aeb510664eb9da7cfaee81b00ee33
Rahul Ohol <rahulohol01@gmail.com>
2023-08-03 23:13:54 +0530

    user model

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: false },
+  googleId: { type: String, required: false },
+  id: { type: String },
+});
+
+const UserModal = mongoose.model("User", userSchema);
+ 
+export default UserModal;
