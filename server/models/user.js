460984e2ae3d34e2c8deb5b6a77c7330f43ebc42
Rahul Ohol <rahulohol01@gmail.com>
2023-08-21 18:07:27 +0530

    user model

--- server/models/user.js
+++ server/models/user.js
@@ -6,9 +6,27 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: false },
   googleId: { type: String, required: false },
   id: { type: String },
+  isVerified: { type: Boolean, default: false },
+  bio: {
+    type: String,
+    default:
+      "Passionate writer and avid reader, sharing my thoughts and stories with the world through words. Exploring diverse topics and perspectives one blog post at a time. 📚✍️",
+  },
   imgpath: {
     type: String,
     required: true,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  followers: {
+    type: [],
+    default: [],
+  },
+  following: {
+    type: [],
+    default: [],
   },
 });
 
