3559b5a9bc8b57f516907a9d447c230f9b67932e
Rahul Ohol <rahulohol01@gmail.com>
2023-09-22 09:23:06 +0530

    update

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use("/comment", CommentRouter);
 
 app.use("/uploads", express.static("./uploads"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
